273383c04130c3b8cfdd06b9b06d7ef17fa8aea6
Jason <jasylwong@gmail.com>
2020-06-07 22:19:14 +0100

    add grid of 9x9

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const createGrid =() => {
-    document.querySelector('grid').appendChild(<div>hello</div>)
-
-  }
+  // const grid = document.querySelector('#grid')
+  // const createGrid = () => {
+  //   for(let i = 1; i < 10; i++ ) {
+  //     const square = document.createElement("div")
+  //     square.id = i
+  //     square.class = 'square'
+  //     grid.appendChild(square)
+  //   }
+  // }
 
   createGrid()
 })
