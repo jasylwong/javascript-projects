56cbcca18490b40c16a4ad28e6bcd0515aafa60b
Jason Wong <jasonwong@jasons-mbp.home>
2020-08-25 22:50:01 +0100

    add colour for decrease and reset buttons

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 WIP
 
 | Project (Repo) | Description |
-| --- | --- | --- |
+| --- | --- |
 | [Portfolio website](https://jasylwong.github.io/javascript-projects/) | The website for showcasing all these individual projects |
 | [Whack-a-mole](https://jasylwong.github.io/javascript-projects/projects/whack-a-mole/index.html) | Based on the classic game, click on as many of the moles as you can in the time limit. Technically my version should be called Whack-a-diglett...
--- projects/counter/app.js
+++ projects/counter/app.js
@@ -9,15 +9,33 @@ const reset = document.getElementById('reset')
 
 dec_btn.onclick = () => {
   counter--
+  color_change()
   display.innerHTML = counter.toString()
 }
 
 inc_btn.onclick = () => {
   counter++
+  if (counter > 0) {
+    display.classList.toggle('positive')
+  } else if (counter === 0) {
+    display.classList.toggle('zero')
+  }
   display.innerHTML = counter.toString()
 }
 
 reset.onclick = () => {
   counter = 0
   display.innerHTML = '0'
+  color_change()
+}
+
+const color_change = () => {
+  display.removeAttribute('class')
+  if (counter > 0) {
+    display.classList.add('positive')
+  } else if (counter === 0) {
+    display.classList.add('zero')
+  } else {
+    display.classList.add('negative')
+  }
 }
