2e63b7057a5d76e5bdabadf83f14cfe08fed7296
jasylwong <jasylwong@gmail.com>
2020-08-27 22:11:13 +0100

    complete tab project

--- projects/tabs/app.js
+++ projects/tabs/app.js
@@ -9,6 +9,10 @@ about.addEventListener('click', function(e) {
       btn.classList.remove('active')
     })
     e.target.classList.add('active')
-    
+    articles.forEach(function(article){
+      article.classList.remove('active')
+    })
+    const element = document.getElementById(id)
+    element.classList.add('active')
   }
 })
